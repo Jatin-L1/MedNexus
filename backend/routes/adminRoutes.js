34699bdc7fe39067a5567ae0dbb888caba11634d
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-02-07 11:11:13 +0530

    professional verification route was added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -24,4 +24,37 @@ router.get("/professional/:type", async (req, res) => {
   }
 });
 
+// * userID = "_id" in MongoDB
+// * type = "isVerifiedDoctor" in MongoDB
+router.post("/professional/set/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { type } = req.body;
+
+    console.log(req);
+
+    if (!["pending", "approved", "rejected"].includes(type)) {
+      return res.status(400).json({ message: "Invalid type parameter" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { isVerifiedDoctor: type },
+      { new: true } // ! Returns updated document back to us
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User verification status updated", user: updatedUser });
+
+  } catch (error) {
+    console.error("Error updating user verification status:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
